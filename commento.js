e68cb258f03e438cb018f4602eebffb31f0dc4bf
LukElvetico <Weber.ltm@gmail.com>
2025-08-03 16:22:19 +0200

    first commit

---
+++ commento.js
@@ -0,0 +1,3 @@
+// Sfortunatamente a causa di una serie di circostanze non sono riuscito a completare la consegna né a seguire il modulo adeguatamente
+// Per quanto mi riguarda vedrò di recuperare il modulo quest'estate approfittando della pausa dal lavoro
+// Chiedo scusa ai docenti
